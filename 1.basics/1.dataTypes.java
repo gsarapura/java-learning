8d387ea1a9b400808db80ca3a8bcaf8eb3b62d60
Gustavo Sarapura <gustavo.sarapura@incluit.com>
2023-10-09 10:21:51 -0300

    [GS] Add basics folder

---
+++ 1.basics/1.dataTypes.java
@@ -0,0 +1,30 @@
+class DataTypes {
+    public static void main(String a[]) {
+        int num1 = 3;
+        short sh = 558;
+        long l = 5854l;
+        byte by = 127; // Limit
+
+        float marks = 6.5f;
+        double dou = 4.6;
+
+        char car = 'h';
+
+        boolean boo = true;
+
+        System.out.println(marks);
+
+        // Literals: any constant value which can be assigned to the variable.
+        // decimal-form literal
+        int al = 101; 
+        // octal-form literal
+        int bl = 0100; 
+        // Hexa-decimal form literal
+        int cl = 0xFace; 
+        // Binary literal
+        int dl = 0b1111; 
+
+        int bigNumber = 10_000_000;
+        System.out.println(bigNumber);
+    }
+}
